678b9a8c017b8a2239a0ca8e8b4daa739b0c93d6
Yi Jie Lim <yijielim95@Yis-MacBook-Air.local>
2025-09-30 12:48:46 +0100

    Added routing and live data from API.

--- movies/src/pages/homepage.jsx
+++ movies/src/pages/homepage.jsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";  
 import Grid from "@mui/material/Grid";
 import MovieList from "../components/movieList";
 
 const HomePage = (props) => {
-  const movies = props.movies;
+    const [movies, setMovies] = useState([]);
 
-  return (
-    <Grid container>
-      <Grid size={12}>
-        <h1> HomePage </h1>
-      </Grid>
-      <Grid container>
-        <MovieList movies={movies}></MovieList>
-      </Grid>
-    </Grid>
-  );
+    useEffect(() => {
+        fetch(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+        )
+        .then((res) => res.json())
+        .then((json) => {
+            // console.log(json);
+            return json.results;
+        })
+        .then((movies) => {
+            setMovies(movies);
+        });
+    }, []);
+
+
+    return (
+        <Grid container>
+        <Grid size={12}>
+            <h1> HomePage </h1>
+        </Grid>
+        <Grid container>
+            <MovieList movies={movies}></MovieList>
+        </Grid>
+        </Grid>
+    );
 };
 export default HomePage;
 
