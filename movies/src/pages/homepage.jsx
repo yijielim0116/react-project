20621324567d9cf8fd8ff3e33ebf1669871a294b
Yi Jie Lim <yijielim95@Yis-MacBook-Air.local>
2025-09-30 12:05:54 +0100

    List of movies displayed.

--- movies/src/pages/homepage.jsx
+++ movies/src/pages/homepage.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
-import MovieCard from '../components/movieCard'
+import MovieList from "../components/movieList";
 
 const HomePage = (props) => {
   const movies = props.movies;
-    
+
   return (
-  <Grid container>
+    <Grid container>
       <Grid size={12}>
-          <h1> HomePage </h1>
+        <h1> HomePage </h1>
       </Grid>
-      <Grid size={3}>
-        <MovieCard movie={movies[0]} />
+      <Grid container>
+        <MovieList movies={movies}></MovieList>
       </Grid>
     </Grid>
   );
 };
 export default HomePage;
+
