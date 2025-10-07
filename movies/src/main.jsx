9a4343e96809c8152efe2959e6725e6884b141dd
Yi Jie Lim <yijielim95@Yis-MacBook-Air.local>
2025-10-07 11:35:31 +0100

    Movie Details Page rendering.

--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import HomePage from "./pages/homepage";
+import MovieDetailsPage from './pages/movieDetailsPage';
 
 const sample = {
   adult: false,
@@ -85,11 +86,19 @@ const sample = {
 
 const movies = [sample, sample, sample, sample, sample, sample, sample];
 
+const images = [
+  "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+  "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
+  "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
+  "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
+]
+
 const App = () => {
   return (
-      <HomePage movies={movies} />
-  );
+      <MovieDetailsPage movie={sample} images={images} />
+      );
 };
+
 
 const rootElement = createRoot( document.getElementById("root") )
 rootElement.render(<App />);
