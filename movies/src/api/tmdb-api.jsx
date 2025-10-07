7cca04cbed334eae07c9fb73cb83823a6052e4bd
Yi Jie Lim <yijielim95@Yis-MacBook-Air.local>
2025-10-07 12:34:34 +0100

    Show movie review excerpts; Refactor Details page.

--- movies/src/api/tmdb-api.jsx
+++ movies/src/api/tmdb-api.jsx
@@ -29,3 +29,14 @@ export const getMovieImages = (id) => {
     .then((res) => res.json())
     .then((json) => json.posters);
 };
+
+export const getMovieReviews = (id) => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${import.meta.env.VITE_TMDB_KEY}`
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      // console.log(json.results);
+      return json.results;
+    });
+};
