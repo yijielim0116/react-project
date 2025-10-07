138b131b52ac7eeb0c1ababab062bff58ba283a7
Yi Jie Lim <yijielim95@Yis-MacBook-Air.local>
2025-10-07 11:43:19 +0100

    Movie Details coming from API.

--- movies/src/components/movieCard/index.jsx
+++ movies/src/components/movieCard/index.jsx
@@ -11,7 +11,8 @@ import CalendarIcon from "@mui/icons-material/CalendarTodayTwoTone";
 import StarRateIcon from "@mui/icons-material/StarRate";
 import IconButton from "@mui/material/IconButton";
 import Grid from "@mui/material/Grid";
-import img from '../../images/film-poster-placeholder.png'
+import img from '../../images/film-poster-placeholder.png';
+import { Link } from "react-router"
 
 export default function MovieCard(props) {
   const movie = props.movie;
@@ -46,9 +47,12 @@ export default function MovieCard(props) {
         <IconButton aria-label="add to favorites" onClick={null}>
           <FavoriteIcon color="primary" fontSize="large" />
         </IconButton>
-        <Button variant="outlined" size="medium" color="primary">
-          More Info ...
-        </Button>
+        <Link to={`/movies/${movie.id}`}>
+          <Button variant="outlined" size="medium" color="primary">
+            More Info ...
+          </Button>
+        </Link>
+
       </CardActions>
     </Card>
   );
